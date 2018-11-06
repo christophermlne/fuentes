d082e6f58092ae6dec63104c90da4e26cb5eed82
Christopher Milne <christopher.milne@gmail.com>
2018-11-06 09:38:50 -0500

    fixes compiler warnings

--- test/fuentes/entry_test.exs
+++ test/fuentes/entry_test.exs
@@ -4,21 +4,20 @@ defmodule Fuentes.EntryTest do
   import Fuentes.TestFactory
   alias Fuentes.{ Amount, Entry }
 
-
-  @valid_attrs params_for(:entry)
-  @invalid_attrs %{}
-
-  @valid_with_amount_attrs %{
-    description: "Purchased a Lamborghini",
-    date: %Ecto.Date{ year: 2016, month: 1, day: 16 },
-    amounts: [ %Amount{ amount: Decimal.new(125000.00), type: "credit", account_id: 1},
-              %Amount{ amount: Decimal.new(125000.00), type: "debit", account_id: 2}]
-    }
+  # @valid_attrs params_for(:entry) # TODO why does this exist?
+  # @invalid_attrs %{} # TODO why does this exist?
+  #
+  # @valid_with_amount_attrs %{
+  #   description: "Purchased a Lamborghini",
+  #   date: %Ecto.Date{ year: 2016, month: 1, day: 16 },
+  #   amounts: [ %Amount{ amount: Decimal.new(125000.00), type: "credit", account_id: 1},
+  #             %Amount{ amount: Decimal.new(125000.00), type: "debit", account_id: 2}]
+  #   } # TODO why does this exist?
 
   test "entry casts associated amounts" do
     changeset = Entry.changeset %Entry{
       description: "Buying first Porsche",
-      date: %Ecto.Date{ year: 2016, month: 1, day: 16 },
+      date: %Ecto.Date{ year: 2016, month: 1, day: 16 }, # TODO use native elixir Date instead
       amounts: [ %Amount{ amount: Decimal.new(125000.00), type: "credit", account_id: 2 },
                  %Amount{ amount: Decimal.new(50000.00), type: "debit", account_id: 1 } ,
                  %Amount{ amount: Decimal.new(75000.00), type: "debit", account_id: 1 } ]
@@ -29,7 +28,7 @@ defmodule Fuentes.EntryTest do
   test "entry debits and credits must cancel" do
     changeset = Entry.changeset %Entry{
       description: "Buying first Porsche",
-      date: %Ecto.Date{ year: 2016, month: 1, day: 16 },
+      date: %Ecto.Date{ year: 2016, month: 1, day: 16 }, # TODO use native elixir Date instead
       amounts: [ %Amount{ amount: Decimal.new(125000.00), type: "credit", account_id: 2 },
                  %Amount{ amount: Decimal.new(50000.00), type: "debit", account_id: 1 } ,
                  %Amount{ amount: Decimal.new(76000.00), type: "debit", account_id: 1 } ]
