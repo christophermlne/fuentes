cb7b57f2620113f7a4b65074b456366ac36ae17e
Christopher Milne <christopher.milne@gmail.com>
2018-11-06 09:37:19 -0500

    adds postgres docker-compose to make development easier

--- config/test.exs
+++ config/test.exs
@@ -5,7 +5,10 @@ config :fuentes, ecto_repos: [Fuentes.TestRepo]
 config :fuentes, Fuentes.TestRepo,
   hostname: "localhost",
   database: "fuentes_test",
+  username: "postgres",
+  password: "postgres",
   adapter: Ecto.Adapters.Postgres,
-  pool: Ecto.Adapters.SQL.Sandbox
+  pool: Ecto.Adapters.SQL.Sandbox,
+  port: 5437
 
 config :logger, level: :warn
